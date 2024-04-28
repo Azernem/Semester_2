e74e7c69edcb3e5a01820a5ce9f519707873e109
Azernem <nematmusaev047.@gmail.com>
2024-04-28 05:09:31 +0300

    router

---
+++ Routers/Routers/Exceptions.cs
@@ -0,0 +1,23 @@
+public class NoSuchWayException: Exception
+{
+    public NoSuchWayException(string message): base(message) 
+    {
+        
+    }
+}
+public class NoEdgeException: Exception
+{
+    public NoEdgeException(string message): base(message) 
+    {
+    }
+}
+
+public class IncorrectExpressionException: Exception
+{
+    public IncorrectExpressionException(string message): base(message) 
+    {
+    }
+}
+
+
+
---
+++ Routers/Routers/Prima.cs
@@ -0,0 +1,121 @@
+using Microsoft.VisualBasic;
+
+public class Graph
+{
+    public int Size {get; set;}
+    public List<List<(int, int)>> graph {get; set;}
+    public List<List<(int, int)>> trie;
+    public List<int> NodesAtTrie;
+    public Graph(int size)
+    {
+        this.Size = size;
+        this.graph = GraphWithSize(size);
+        this.trie = GraphWithSize(size);
+        this.NodesAtTrie = new List<int>();
+    }
+    /// <summary>
+    /// creates graph with size
+    /// </summary>
+    /// <param name="size">size of graph, topology</param>
+    /// <returns>topology at graph</returns>
+    public List<List<(int, int)>> GraphWithSize(int size)
+    {
+        List<List<(int, int)>> graph = new();
+        for (int i =0; i<size; ++i)
+        {
+            var list = new List<(int, int)>();
+            graph.Add(list);
+        }
+        return graph;
+    }
+    /// <summary>
+    /// method that to  new router-edges to graph 
+    /// </summary>
+    /// <param name="firstNode">first node</param>
+    /// <param name="secondNode">second node</param>
+    /// <param name="weight">power of edge</param>
+    public void NewEdge(int firstNode, int secondNode, int weight)
+    {
+        trie[firstNode].Add((secondNode, weight));
+        trie[secondNode-1].Add((firstNode+1, weight));
+    }
+    /// <summary>
+    /// checking to useless nodes
+    /// </summary>
+    /// <exception cref="NoEdgeException">exception if empty nodes</exception>
+    public void EmptyNodes()
+    {
+        foreach (List<(int, int)> list in graph)
+        {
+            if (list.Count == 0 )
+            {
+                throw new NoEdgeException("Tere is Node, which doesnt contain edge");
+            }
+        }
+    }
+    /// <summary>
+    /// searcing of maximal weight? power of edge
+    /// </summary>
+    public void AddMaxWeight()
+    {
+        EmptyNodes();
+        var max = 0;
+        int firstNode =0, secondNode = 0;
+        for (int i = 0; i<graph.Count; ++i)
+        {
+            foreach((int, int) couple in graph[i])
+            {
+                if (couple.Item2>max)
+                {
+                    max = couple.Item2;
+                    secondNode = couple.Item1;
+                    firstNode = i;
+                }
+            }
+        }
+        NewEdge(firstNode, secondNode, max);
+        NodesAtTrie.Add(firstNode+1);
+        NodesAtTrie.Add(secondNode);
+    }
+    /// <summary>
+    /// crates right topology-graph 
+    /// </summary>
+    /// <returns>graph without extra edges</returns>
+    public List<List<(int, int)>> Prima()
+    {
+        AddMaxWeight();
+        int max = 0;
+        int candidatToTrieOne = 0;
+        int candidatToTrietwo = 0;
+        while (NodesAtTrie.Count!=Size)
+        {
+            for (int Node = 0; Node<graph.Count; Node++)
+            {
+                if (NodesAtTrie.Contains(Node+1))
+                {
+                    foreach((int, int) couple in graph[Node])
+                    {
+                        if (!(NodesAtTrie.Contains(couple.Item1)))
+                        {
+                            if (couple.Item2>max)
+                            {
+                                max = couple.Item2;
+                                candidatToTrietwo = couple.Item1;
+                                candidatToTrieOne = Node;
+                            }
+                        }
+                        else{
+                            continue;
+                        }
+                    }
+                }
+                else{
+                    continue;
+                }
+            }
+            NewEdge(candidatToTrieOne, candidatToTrietwo, max);
+            NodesAtTrie.Add(candidatToTrietwo);
+        }
+        return trie;
+    }
+}
