5877039c1832412543d448f6f03912db202a2104
Azernem <nematmusaev047.@gmail.com>
2024-05-03 16:30:46 +0300

    with style guide

--- Map_Filter_Fold/Map_Filter_Folder/MapFilterFold.cs
+++ Map_Filter_Fold/Map_Filter_Folder/MapFilterFold.cs
@@ -11,25 +11,25 @@ public class Functions
     /// <typeparam name="Tres">type of element called to the function</typeparam>
     /// <param name="value">valгу that converted to Tres type</param>
     /// <returns>converted value with Tres type </returns>
-    public delegate Tres F_for_Map<T, Tres>(T value);
+    public delegate Tres FforMap<T, Tres>(T value);
     /// <summary>
     /// 
     /// </summary>
     /// <typeparam name="T">type of elements of current list</typeparam>
     /// <typeparam name="Tres">type of elements of result list</typeparam>
-    /// <param name="list_start">current list</param>
+    /// <param name="liststart">current list</param>
     /// <param name="fun">function that convertes element of list with Tres type</param>
     /// <returns>converted list</returns>
-    public List<Tres> Map<T, Tres>(List<T> list_start, F_for_Map<T, Tres> fun)
+    public List<Tres> Map<T, Tres>(List<T> liststart, FforMap<T, Tres> fun)
     {
-        if (list_start == null)
+        if (liststart == null)
         {
             throw new ArgumentNullException("empty list");
         }
         List<Tres> result = [];
-        for (int i = 0; i<list_start.Count; ++i)
+        for (int i = 0; i<liststart.Count; ++i)
         {
-            result.Add(fun(list_start[i]));
+            result.Add(fun(liststart[i]));
         }
         return result;
     } 
@@ -39,31 +39,31 @@ public class Functions
     /// <typeparam name="T">type of type of element supplied to the function</typeparam>
     /// <param name="value">element of current list</param>
     /// <returns>boolean to condition</returns>
-    public delegate bool F_for_Filter<T>(T value);
+    public delegate bool FforFilter<T>(T value);
     /// <summary>
     /// method that return filtered list
     /// </summary>
     /// <typeparam name="T">type of current list</typeparam>
-    /// <param name="list_start"> currrent list</param>
+    /// <param name="liststart"> currrent list</param>
     /// <param name="fun">function that returns boolean to condition on conditional to element of list</param>
     /// <returns>filtered list</returns>
-    public List<T> Filter<T>(List<T> list_start, F_for_Filter<T> fun)
+    public List<T> Filter<T>(List<T> liststart, FforFilter<T> fun)
     {
-        if (list_start == null)
+        if (liststart == null)
         {
             throw new ArgumentNullException("empty list");
         }
         var res = new List<T>();
-        for (int i = 0; i < list_start.Count; ++i)
+        for (int i = 0; i < liststart.Count; ++i)
         {
-            if (fun(list_start[i]))
+            if (fun(liststart[i]))
             {
-                res.Add(list_start[i]);
+                res.Add(liststart[i]);
             }
         }
         return res;
     }
-    public delegate Tres F_for_Fold<T, Tres>(Tres acc, T elem);
+    public delegate Tres FforFold<T, Tres>(Tres acc, T elem);
     /// <summary>
     /// method that returns a value that changes depending on the elements of the list
     /// </summary>
@@ -73,7 +73,7 @@ public class Functions
     /// <param name="acc">result</param>
     /// <param name="fun">returnes Tres value</param>
     /// <returns>value acc that depending on elementes of list</returns>
-    public Tres Fold<T, Tres>(List<T> list, Tres acc, F_for_Fold<T, Tres> fun)
+    public Tres Fold<T, Tres>(List<T> list, Tres acc, FforFold<T, Tres> fun)
     {
         if (list == null)
         {
