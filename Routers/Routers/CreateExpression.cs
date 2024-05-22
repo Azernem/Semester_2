1d292dde29219fb66a735a9d8dc9ad11deaa50ce
Azernem <nematmusaev047.@gmail.com>
2024-05-22 21:12:06 +0300

    until not normal change

--- Routers/Routers/CreateExpression.cs
+++ Routers/Routers/CreateExpression.cs
@@ -13,19 +13,23 @@ public class GraphToExpression
         Graph.Prima();
         var trie = Graph.trie;
         var result = new List<string>();
+
         for (int line = 0; line<trie.Count; ++line)
         {
             if (trie[line].Count>1)
             {
-            string s = $"{line+1}: ";
-            foreach((int, int) couple in trie[line])
-            {
-                s+= $"{couple.Item1} ({couple.Item2}), ";
-            } 
-            s = s.Substring(0, s.Length - 2);
-            result.Add(s);
+                string s = $"{line+1}: ";
+
+                foreach((int, int) couple in trie[line])
+                {
+                    s+= $"{couple.Item1} ({couple.Item2}), ";
+                } 
+
+                s = s.Substring(0, s.Length - 2);
+                result.Add(s);
             }
         }
+        
         File.WriteAllLines(way, result.ToArray());
     }
 }
--- Routers/Routers/Prima.cs
+++ Routers/Routers/Prima.cs
@@ -6,13 +6,110 @@ public class Graphe
     public List<List<(int, int)>> graph {get; set;}
     public List<List<(int, int)>> trie;
     public List<int> NodesAtTrie;
-    public Graphe(int size)
+
+    public Graphe(string path)
     {
-        this.Size = size;
-        this.graph = GraphWithSize(size);
-        this.trie = GraphWithSize(size);
+        this.Size = MaxNode(path);
+        this.graph = GraphWithSize(Size);
+        this.trie = GraphWithSize(Size);
         this.NodesAtTrie = new List<int>();
     }
+
+    /// <summary>
+    /// method wich gets graph from input expression
+    /// </summary>
+    /// <param name="path">filepath</param>
+    /// <returns></returns>
+    /// <exception cref="NoSuchpathException">throw exseption if file doesnt exist </exception>
+    public void Getgraph(string path)
+    {
+        if (!(File.Exists(path)))
+        {
+            throw new NoSuchpathException("No such file");
+        }
+
+        string[] expression = File.ReadAllLines(path);
+
+        for (int i = 0; i<expression.Length; ++i)
+        {
+            var stringDependendNodes = expression[i].Substring(3, expression[i].Length - 4);
+            stringDependendNodes = stringDependendNodes.Replace("(", "").Replace(")", "").Replace(", ", ",");
+            var couples = GetCouplesDependendNodes(stringDependendNodes);
+
+            foreach ((int, int) couple in couples)
+            {
+                graph[i].Add(couple);
+                graph[couple.Item1 - 1].Add((i+1, couple.Item2));
+            }
+        }
+
+        this.EmptyNodes();
+    }
+
+    /// <summary>
+    /// method that returns a max number router (emount of all routers at topology)
+    /// </summary>
+    /// <param name="path">filepath with topology-text</param>
+    /// <returns>emount of all routers at topology</returns>
+    /// <exception cref="NoSuchpathException">throw exseption if file doesnt exist</exception>
+    public int MaxNode(string path)
+    {
+        int max = 0;
+
+        if (!(File.Exists(path)))
+        {
+            throw new NoSuchpathException("No such file");
+        }
+
+        string[] expression = File.ReadAllLines(path);
+
+        for (int i = 0; i<expression.Length; ++i)
+        {
+            var stringDependendNodes = expression[i].Substring(3, expression[i].Length - 4);
+            stringDependendNodes = stringDependendNodes.Replace("(", "").Replace(")", "").Replace(", ", ",");
+            var couples = GetCouplesDependendNodes(stringDependendNodes);
+
+            foreach ((int, int) couple in couples)
+            {
+                if (couple.Item1>max)
+                {
+                    max = couple.Item1;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    /// <summary>
+    /// method transformes topology-text to array of couples
+    /// </summary>
+    /// <param name="s">line of file</param>
+    /// <returns>array of int couples</returns>
+    /// <exception cref="IncorrectExpressionException">incorrect topology</exception>
+    public (int, int)[] GetCouplesDependendNodes(string s)
+    {
+        var array = s.Split(",");
+        var result = new (int, int)[array.Length];
+
+        for (int i =0; i<array.Length; ++i)
+        {
+            var arraypara = array[i].Split(" ");
+            int numbernode, numberweight;
+
+            if (int.TryParse(arraypara[0], out numbernode) && int.TryParse(arraypara[1], out numberweight))
+            {
+                result[i] = (numbernode, numberweight);
+            }
+            else{
+                throw new IncorrectExpressionException(" router isnt number in expression");
+            }
+        }
+
+        return result;
+
+    }
+
     /// <summary>
     /// creates graph with size
     /// </summary>
@@ -21,11 +118,13 @@ public class Graphe
     public List<List<(int, int)>> GraphWithSize(int size)
     {
         List<List<(int, int)>> graph = new();
+
         for (int i =0; i<size; ++i)
         {
             var list = new List<(int, int)>();
             graph.Add(list);
         }
+
         return graph;
     }
     /// <summary>
@@ -61,6 +160,7 @@ public class Graphe
         EmptyNodes();
         var max = 0;
         int firstNode =0, secondNode = 0;
+
         for (int i = 0; i<graph.Count; ++i)
         {
             foreach((int, int) couple in graph[i])
@@ -73,6 +173,7 @@ public class Graphe
                 }
             }
         }
+
         NewEdge(firstNode, secondNode, max);
         NodesAtTrie.Add(firstNode+1);
         NodesAtTrie.Add(secondNode);
@@ -87,6 +188,7 @@ public class Graphe
         int max = 0;
         int candidatToTrieOne = 0;
         int candidatToTrietwo = 0;
+
         while (NodesAtTrie.Count!=Size)
         {
             for (int Node = 0; Node<graph.Count; Node++)
@@ -104,18 +206,14 @@ public class Graphe
                                 candidatToTrieOne = Node;
                             }
                         }
-                        else{
-                            continue;
-                        }
                     }
                 }
-                else{
-                    continue;
-                }
             }
+
             NewEdge(candidatToTrieOne, candidatToTrietwo, max);
             NodesAtTrie.Add(candidatToTrietwo);
         }
+        
         return trie;
     }
 }
