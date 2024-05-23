a0f3e15940a2b923ed3b97715163d3cf105778aa
Azernem <nematmusaev047.@gmail.com>
2024-05-24 00:15:57 +0300

    comment to licensy

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Form1.cs" company="NematMusaev"> 
+// Copyright (c) PlaceholderCompany. All rights reserved. 
+// </copyright>
+namespace CatchTheButton;
+
+/// <summary>
+/// Catch a button.
+/// </summary>
+public partial class Form1 : Form
+{
+    private Random random = new Random();
+
+    /// <summary>
+    /// Form constructor
+    /// </summary>
+
+    private void MouseMoveUnderButton(object sender, MouseEventArgs e)
+    {
+        var PositionButton = new Point(random.Next(Width - button.Width), random.Next(Height - button.Height));
+
+        while (Math.Abs(Control.MousePosition.X - PositionButton.X) < 150 || Math.Abs(Control.MousePosition.Y - PositionButton.Y) < 50)
+        {
+            PositionButton = new Point(random.Next(Width - button.Width - 50),
+            random.Next(Height - button.Height - 50));
+        }
+        button.Location = PositionButton;
+    }
+
+    private void ClickButton(object sender, EventArgs e)
+    {
+        Close();
+    }
+    /// <summary>
+    /// initialise.
+    /// </summary>
+    public GameFrom()
+    {
+        InitializeComponent();
+    }
+}
