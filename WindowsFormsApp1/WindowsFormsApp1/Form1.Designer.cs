c2c6f63cfd24eae84b3d10f1ad04aebbf2dcdfd2
Azernem <nematmusaev047.@gmail.com>
2024-05-24 00:15:57 +0300

    comment to licensy

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="Form1.Designer.cs" company="NematMusaev"> 
+// Copyright (c) PlaceholderCompany. All rights reserved. 
+// </copyright>
+namespace CatchTheButton;
+
+partial class GameFrom
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>s
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">Need to eliminate managed tasks</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Immutable method for Designer support
+    /// </summary>
+    private void InitializeComponent()
+    {
+        button = new Button();
+        // 
+        // button
+        // 
+        button.Location = new Point(200, 200);
+        button.Name = "button";
+        button.Size = new Size(100, 100);
+        button.TabIndex = 0;
+        button.UseVisualStyleBackColor = true;
+        button.MouseMove += MouseMoveUnderButton;
+        button.Click += ClickButton;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 453);
+        Controls.Add(button);
+        MinimumSize = new Size(500, 500);
+        Name = "Form1";
+        Text = "Catch the button";
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Button button;
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="NematMusaev"> 
+// Copyright (c) PlaceholderCompany. All rights reserved. 
+// </copyright>
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
