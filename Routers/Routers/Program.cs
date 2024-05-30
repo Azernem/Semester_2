3a2f77732ecdf828106ba7a9adbcec5e04c80463
Azernem <nematmusaev047.@gmail.com>
2024-05-30 18:30:36 +0300

    style guide the better

--- Routers/Routers/Program.cs
+++ Routers/Routers/Program.cs
@@ -1,16 +1,6 @@
-﻿// <copyright file="Program.cs" company="NematMusaev">
+﻿﻿// <copyright file="Program.cs" company="NematMusaev">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
-using System.Data;
 
-internal static class Programm
-{
-    public static void Main(string[] argc)
-    {
-        /*string way = "Edges_of_Routers_.txt";*/
-        string way = "Routers/Edges_of_Routers_.txt";
-        string toWay = "Routers/Result_File.txt" ;
-        var createexpression = new GraphToExpression();
-        createexpression.CreateExpression(way, toWay);
-    }
-}
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
