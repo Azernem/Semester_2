907e73a1c6d51573a52464810f6c53601d4f5859
Azernem <nemat.musaev5@mail.ru>
2025-09-10 00:11:18 +0400

    Revert "Merge pull request #9 from Azernem/Routers_"
    This reverts commit 7f2854c037918c5998ef17cb890e7d102b1fe1a4, reversing
    changes made to 36ef2936a86faa69d3e61e520775d00247f15abb.

--- Routers/Routers/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿﻿// <copyright file="Program.cs" company="NematMusaev">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
